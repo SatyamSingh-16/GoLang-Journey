6bd92bb45480e516e5e0b67f903d6bef57d48785
Satyam Singh <satyamsing9e@gmail.com>
2026-07-13 14:32:54 +0530

    Learned about packages and it's Structure

---
+++ Day 8/calculator/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"calculator/math" //Import Path = Module Name + Package Path
+)
+
+func main() {
+
+	result := math.Add(10,20)
+
+	fmt.Println(result)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-journey
+
+go 1.26.4
