1e9e33f1746a442af9ebef5c5d4589acf8f9e040
Satyam Singh <satyamsingh9e@gmail.com>
2026-07-31 09:14:16 +0530

    Learned dynamic routing

--- Day-26/student-api-gin/main.go
+++ Day-26/student-api-gin/main.go
@@ -7,7 +7,25 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello Buddy!")
+		c.String(200, "Home Page")
+	})
+
+	router.GET("/about", func(c *gin.Context) {
+		c.String(200, "About Page")
+	})
+
+	router.GET("/contact", func(c *gin.Context) {
+		c.String(200, "Contact Page")
+	})
+	router.GET("/students/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		c.String(200, "Student ID: "+id)
+	})
+	router.GET("/students/:id/:courses/:courseId", func(c *gin.Context) {
+		studentID := c.Param("id")
+		courseID := c.Param("courseId")
+		course := c.Param("courses")
+		c.String(200, "Student "+studentID+" "+course+" "+courseID)
 	})
 	router.Run(":8080")
 
