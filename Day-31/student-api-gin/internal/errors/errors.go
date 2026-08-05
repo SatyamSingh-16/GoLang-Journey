359053336ad4a3e043c439ac53fa2164fa4233f6
Satyam Singh <satyamsingh9e@gmail.com>
2026-08-05 08:54:58 +0530

    Implemented services and repo

--- Day-31/student-api-gin/internal/errors/errors.go
+++ Day-31/student-api-gin/internal/errors/errors.go
@@ -3,3 +3,6 @@ package errors
 import "errors"
 
 var ErrStudentNotFound = errors.New("student not found")
+var ErrEmailAlreadyExists = errors.New("email already exists")
+
+var ErrInvalidCredentials = errors.New("invalid credentials")
---
+++ Day-31/student-api-gin/internal/service/auth_service.go
@@ -0,0 +1,76 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"student-api-gin/internal/dto"
+	customerrors "student-api-gin/internal/errors"
+	"student-api-gin/internal/models"
+	"student-api-gin/internal/repository"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService struct {
+	repo *repository.AuthRepository
+}
+
+func NewAuthService(
+	repo *repository.AuthRepository,
+) *AuthService {
+
+	return &AuthService{
+		repo: repo,
+	}
+}
+func (s *AuthService) Register(
+	ctx context.Context,
+	request dto.RegisterRequest,
+) (*dto.RegisterResponse, error) {
+	existingUser, err := s.repo.GetUserByEmail(
+		ctx,
+		request.Email,
+	)
+
+	if err != nil {
+
+		if !errors.Is(err, sql.ErrNoRows) {
+			return nil, err
+		}
+
+	}
+
+	if existingUser != nil {
+		return nil, customerrors.ErrEmailAlreadyExists
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword(
+		[]byte(request.Password),
+		bcrypt.DefaultCost,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	user := models.User{
+		Name:         request.Name,
+		Email:        request.Email,
+		PasswordHash: string(hashedPassword),
+		Role:         "student",
+	}
+	err = s.repo.CreateUser(
+		ctx,
+		&user,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	response := dto.RegisterResponse{
+		ID:      user.ID,
+		Name:    user.Name,
+		Email:   user.Email,
+		Message: "User registered successfully",
+	}
+	return &response, nil
+}
